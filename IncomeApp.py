35a6279ea0f19b337b1c0c78a41cc98059b3bd55
Ajeeba <ajeebahaneefa@gmail.com>
2025-03-10 15:55:03 +0530

    Update IncomeApp.py

--- IncomeApp.py
+++ IncomeApp.py
@@ -45,7 +45,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title(<h1>'Income Prediction App'</h1>)
+st.markdown(<h1>'Income Prediction App'</h1>, unsafe_allow_html=True)
 
 st.markdown("""
     <style>
