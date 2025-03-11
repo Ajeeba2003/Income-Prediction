e62075bd3f410a9f2118f9d2658446ca031bb98b
Ajeeba <ajeebahaneefa@gmail.com>
2025-03-11 11:30:29 +0530

    Update IncomeApp.py

--- IncomeApp.py
+++ IncomeApp.py
@@ -21,7 +21,7 @@ Dte=joblib.load("decision_tree.pkl")
 
 st.markdown("""
     <style>
-        div[data-testid="stApp"] { background-color: #b8e3a9; 
+        [data-testid="stApp"] { background-color: #b8e3a9; 
             }   
         .stButton>button { background-color: #b8e3a9; 
         color: white; 
